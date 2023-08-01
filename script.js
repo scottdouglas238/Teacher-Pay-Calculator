357a08ef1ed1463d7fa8097c116ae3a454ce6841
scottdouglas238 <scottdouglas238@gmail.com>
2023-07-31 19:50:40 -0700

    Date calculator working (days between 1/1 or current year and date selected)

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const ytd = document.getElementById("ytd")
 const payFrequency  = document.getElementById("payFrequency")
-const  lastDate = document.getElementById("lastDate")
+const lastDate = document.getElementById("lastDate");
 const elapsedPayPeriods  = document.getElementById("elapsedPayPeriods")
 const button = document.getElementById("button")
 const numOfMonths = document.getElementById("numOfMonths")
 const teacherPayTotal = document.getElementById("teacherPayTotal")
-
-    button.addEventListener("click", function(e){
-        e.preventDefault()
-        teacherPayTotal.innerHTML = ""
-        
-
-       
-        const total = parseFloat(ytd.value)/parseFloat(elapsedPayPeriods.value) * parseFloat(numOfMonths.value)
-    teacherPayTotal.appendChild(document.createTextNode(parseFloat(total)))
-        
-    console.log(parseFloat(total))
-
-    })
+let PFarray = []
 
 
 
+button.addEventListener("click", function(e){
+    e.preventDefault()
+    const currentDate = new Date().getFullYear()
+    const date1 = new Date(currentDate, 0, 1)
+    const dateParts = lastDate.value.split("-")
+    const date2 = new Date(dateParts[0], dateParts[1] - 1, dateParts[2])
+    const diffInMilliseconds = date2 - date1;
+    const diffInDays = diffInMilliseconds / 1000 / 60 / 60 / 24;
+ 
+    
+
+    console.log(diffInDays)
+    
+})
+
+
+
+
+payFrequency.addEventListener("click", function(){
+   PFarray = []
+   const PFV = payFrequency.options.selectedIndex
+   PFarray.push(PFV)
+
+
+})
